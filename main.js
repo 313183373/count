641b1fcf8e6d0df4f0ebcc2fb2498d56a9c85a48
313183373 <313183373@qq.com>
2017-08-20 18:16:06 +0800

    fix bug

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ function count(arr) {
 	let map=new Map();
 	arr=arr.split('');
 	arr.forEach(value=>map.set(value,map.has(value)?map.get(value)+1:1));
-	Array.from(map).sort((a,b)=>b[1]-a[1]).filter((value,index,arr)=>value[1]===arr[0][1]).forEach(value=>{console.log( {[value[0]]:value[1]})});
+	let result={};
+	Array.from(map).sort((a,b)=>b[1]-a[1]).filter((value,index,arr)=>value[1]===arr[0][1]).forEach(value=>{result[value[0]]=value[1]});
+	console.log(result);
 }
 
 count('aaaaabbdbdbb');
